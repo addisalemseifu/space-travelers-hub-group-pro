ba39a53ff72214f5c8546a0e2caf5b7f6d35ee5c
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-05-23 16:46:21 +0300

    Fix linters.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import planet from './planet.png'
+import planet from './planet.png';
 
 export default function Navigation() {
   return (
     <nav>
       <img src={planet} alt="" />
-      {/* <h3>Space Travelers' Hub</h3>
-      <li className='links'
-     ><NavLink to="/" ClassName='link'
-      ><span className='my-link'>Rockets</span></NavLink></li>
-      <li><NavLink to="/missions"><span className='my-link'>Missions</span> </NavLink></li>
-      <span className='ver-line'></span>
-      <li><NavLink to="/myprofile"><span className='my-link'>MY Profile</span></NavLink></li> */}
-      
     </nav>
   );
 }
