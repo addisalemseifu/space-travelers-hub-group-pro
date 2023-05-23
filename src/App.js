5f820b72ddac370429ac7f3a002eff7b699ff6c3
Emmanuel <emmanuelnwoye12@gmail.com>
2023-05-23 17:57:25 +0100

    Add profile section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Profile from './components/profile';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navigation />
+      <div className="App">
+        <Routes>
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div className="pro-container">
+      <div>
+        <h3>My Missions</h3>
+      </div>
+      <div>
+        <h3>My Rockets</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
