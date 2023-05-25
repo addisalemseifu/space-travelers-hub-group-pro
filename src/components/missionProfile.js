23db040879eaf2cb319652e5f28f9bd601285bd0
Emmanuel <emmanuelnwoye12@gmail.com>
2023-05-25 02:12:04 +0100

    Add h3 tag

--- src/components/missionProfile.js
+++ src/components/missionProfile.js
@@ -8,6 +8,7 @@ const MissionProfile = () => {
   return (
     <div className="reserve-wrapper">
       <div className="reserve-container">
+        <h3>My Missions</h3>
         { missions.length === 0 && <p>You have no reserved missions</p>}
         {missions.map((mission) => (
           <h3 key={mission.mission_id} className="reserve-item">{mission.mission_name}</h3>
