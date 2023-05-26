6328489be99941a3c730569ceb44e3f590e1f6b0
Emmanuel <emmanuelnwoye12@gmail.com>
2023-05-26 14:16:08 +0100

    Update js files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import logger from 'redux-logger';
 import missions from './missions/missionSlice';
 import dragonsReducer from './dragons/dragonSlice';
 import rocketReducer from './rockets/rocketsSlice';
@@ -10,7 +9,6 @@ const store = configureStore({
     dragons: dragonsReducer,
     rocket: rocketReducer,
   },
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 
 });
 
